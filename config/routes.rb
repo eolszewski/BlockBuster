11cffd4ef0d5d48c2d6ad611b733aa8516a55ae5
ericolszewski <eolszewski@gmail.com>
2014-08-13 13:17:08 -0500

    Add fontawesome

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Blockbuster::Application.routes.draw do
+
+  root :to => 'posts#index'
+
   resources :posts do
     resources :comments, :only => [:create] do
       member do
@@ -11,6 +14,7 @@ Blockbuster::Application.routes.draw do
       post "dislike", to: "posts#downvote"
     end
   end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
